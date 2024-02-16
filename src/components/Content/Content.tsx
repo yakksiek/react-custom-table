9f32a1a8160ab70e5f21571360aafeb8733ff8e1
MarcinK <marcin.kulbicki@gmail.com>
2024-02-16 12:59:20 +0100

    feat(Content) add types

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import * as t from '../../models/interfaces';
+
 const StyledTableCell = styled.td`
     border: 1px solid #ddd;
     text-align: left;
     padding: 8px;
 `;
 
-function Content({ entries, columns }) {
-    const renderContent = entries => {
+interface ContentProps {
+    entries: t.User[];
+    columns: t.HeaderCell[];
+}
+
+function Content({ entries, columns }: ContentProps) {
+    const renderContent = (entries: t.User[]) => {
         return (
             <tbody>
                 {entries.map(item => {
@@ -16,7 +23,7 @@ function Content({ entries, columns }) {
                         <tr key={item.id}>
                             {columns.map(column => {
                                 const { field } = column;
-                                return <StyledTableCell key={field}>{item[field]}</StyledTableCell>;
+                                return <StyledTableCell key={field}>{item[field as keyof t.User]}</StyledTableCell>;
                             })}
                         </tr>
                     );
